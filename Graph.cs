85920e29f7abc3b99b93e3dbd066fb2555235ea1
Fichdeveloper <33698787+Fichdeveloper@users.noreply.github.com>
2018-04-28 16:07:15 +0300

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework_C_Sharp
+{
+    class Graph
+    {
+        private int[,] list;
+        private int n;
+        int counter = 0;
+        int edges=0;
+        public Graph(int n)
+        {
+            list = new int[n, n];
+            this.n = n;
+        }
+
+        public Graph(int[,]mass,int edges)
+        {
+            list = mass;
+            this.n = mass.GetLength(0);
+            this.edges = edges;
+        }
+
+        public void Add(int from,int to, int cost)
+        {
+            list[from, to] = cost;
+        }
+
+        public void DFS(int st, int[] visited)
+        {       
+            visited[st] = 1;
+            counter++;
+
+            for (int i=0;i<n;i++)
+            {
+                if (list[st,i]==1 && visited[i]==0)
+                {
+                    DFS(i,visited);
+                }
+            }           
+        }
+
+        public void CheckGraph()
+        {
+            DFS(0,new int [n]);
+            edges /= 2;
+            
+            if ((edges == n - 1)&&(counter==n))
+                Console.WriteLine("The graph is a tree");
+            else
+                Console.WriteLine("The graph isn't a tree");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of edges:");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int edges = 0;
+            int[,] arr = new int[a,a];
+
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the adjacency matrix");
+
+            for (int i=0;i<a;i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    arr[i,j] = Convert.ToInt32(Console.ReadLine());
+                    edges += arr[i,j];
+                }
+            }
+
+            Graph graph = new Graph(arr,edges);
+            graph.CheckGraph();
+        }
+    }
+}
